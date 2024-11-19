9b96d8e21d0360df77a3b8d9e3ef8447a9a713a6
rlatjrqja <rlatjrqja@yuhan.ac.kr>
2024-11-19 15:59:24 +0900

    Json 프로토콜 추가

--- Protocol/Protocol2_Plain.cs
+++ Protocol/Protocol2_Plain.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace Protocols
 {
-    internal class Protocol2_Plain
+    public class Protocol2_Plain
     {
+        public static string? GetOneMessage(ref string data)
+        {
+            int startIndex = data.IndexOf('\"');
+            int endIndex = data.IndexOf('\"') + 1;
+            if (startIndex < 0 || endIndex <= 0) return null;
+
+            string message = data.Substring(startIndex, endIndex);
+
+            data = data.Substring(endIndex);
+            return message;
+        }
+
     }
 }
---
+++ Protocol/Protocol3_Json.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Protocols
+{
+    public class Protocol3_Json
+    {
+        public static JsonDocument StringToJson(byte[] data)
+        {
+            // byte[]를 문자열로 변환
+            string jsonString = Encoding.UTF8.GetString(data);
+
+            // JSON 문자열을 객체로 역직렬화
+            var jsonObject = JsonSerializer.Deserialize<dynamic>(jsonString);
+
+
+            return jsonObject;
+        }
+
+        public static byte[] JsonToByte(JsonDocument jsonObject)
+        {
+            // JSON 객체를 문자열로 변환
+            string jsonString = JsonSerializer.Serialize(jsonObject);
+
+            // 문자열을 byte[]로 변환
+            byte[] byteArray = Encoding.UTF8.GetBytes(jsonString);
+
+
+            return byteArray;
+        }
+    }
+}
